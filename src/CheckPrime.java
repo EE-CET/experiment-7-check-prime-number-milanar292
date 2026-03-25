af038898eb5e98a5d2821dc957177f7e7cca9cdd
milanar292 <milanar292@gmail.com>
2026-03-25 12:12:38 +0530

    Update CheckPrime.java

--- src/CheckPrime.java
+++ src/CheckPrime.java
@@ -1,11 +1,21 @@
+import java.util.Scanner;
 public class CheckPrime {
-   
-        // TODO: Read n
-        // TODO: Check if n is prime
-        // Hint:
-        // 1. If n <= 1, it is Not Prime.
-        // 2. Loop from 2 to sqrt(n). If n is divisible by any number, it is Not Prime.
-        // 3. Otherwise, it is Prime.
-        // TODO: Print "Prime" or "Not Prime"
-    
+   public static boolean isPrime(int n){
+      if (n<=1) return false;
+      for (int i=2;i<=Math.sqrt(n);i++){
+         if (n%i==0) return false;
+      }
+      return true;
+   }
+   public static void main(String args[]){
+      Scanner sc=snew Scanner(System.in);
+      int n = sc.nextInt();
+      if (isPrime(n)) {
+            System.out.println("Prime");
+      } else {
+            System.out.println("Not Prime");
+        }
+    }
 }
+      
+   

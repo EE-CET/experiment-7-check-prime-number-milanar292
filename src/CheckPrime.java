790ef2ff74eedfb081e3fdf89aa86e05f4ce87cb
milanar292 <milanar292@gmail.com>
2026-04-04 21:41:36 +0530

    Update CheckPrime.java

--- src/CheckPrime.java
+++ src/CheckPrime.java
@@ -1,21 +1,48 @@
 import java.util.Scanner;
-public class CheckPrime {
-   public static boolean isPrime(int n){
-      if (n<=1) return false;
-      for (int i=2;i<=Math.sqrt(n);i++){
-         if (n%i==0) return false;
-      }
-      return true;
-   }
-   public static void main(String args[]){
-      Scanner sc=snew Scanner(System.in);
-      int n = sc.nextInt();
-      if (isPrime(n)) {
+
+class PrimeChecker {
+    private int number;
+
+    // Constructor to initialize the number
+    public PrimeChecker(int number) {
+        this.number = number;
+    }
+
+    // Method containing the prime logic
+    public boolean isPrime() {
+        if (number <= 1) return false;
+        
+        // Loop from 2 to the square root of n
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Method to display the output
+    public void displayResult() {
+        if (isPrime()) {
             System.out.println("Prime");
-      } else {
+        } else {
             System.out.println("Not Prime");
         }
     }
 }
-      
-   
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // Input
+        int n = sc.nextInt();
+        
+        // Creating an object and calling methods
+        PrimeChecker pc = new PrimeChecker(n);
+        pc.displayResult();
+        
+        sc.close();
+    }
+}
+
